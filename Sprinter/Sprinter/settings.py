6c421994a350c9c01df59039aabb3c4f28de6748
Артур Сагадеев <arturik_sagadeev@mail.ru>
2024-06-02 17:44:32 +0500

    Add .gitignore and add requirements.txt

--- Sprinter/Sprinter/settings.py
+++ Sprinter/Sprinter/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SprinterReact',
+    'SpriterReact',
     'rest_framework',
     'corsheaders',
     'django_filters',
